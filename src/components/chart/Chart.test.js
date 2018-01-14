0d0bac0f86671edd56606b47dafa7cefadbf8193
Dániel Kántor <git@daniel-kantor.com>
2018-01-14 09:16:48 +0100

    refactored tests

--- src/components/chart/Chart.test.js
+++ src/components/chart/Chart.test.js
@@ -7,33 +7,40 @@ var jsdom = require('mocha-jsdom')
 describe('ChartComponent', function() {
   jsdom({'useEach': true})
 
-  it('billboard called', function() {
+  const call_render_with = (args) => {
     const fake_generate = sinon.spy()
     const ChartComponent = ChartComponentInjector({'billboard.js': {'bb': {'generate': fake_generate}}}).default
-    const bind = ChartComponent({'type': 'spline'})
+    const bind = ChartComponent(args.component_args)
     const d3 = require('d3')
-    const render = bind(d3.selection())
-    render({'columns': [
-      ['x', 1, 2, 3],
-      ['y', 1, 2, 3],
-    ]})
+    const selection = d3.selection()
+    selection.append = () => ({'node': () => 'magic'})
+    const render = bind(selection)
+    render(args.render_args)
+
+    return { fake_generate, selection }
+  }
+
+  it('billboard called', function() {
+    const { fake_generate } = call_render_with({
+      "component_args": {'type': 'spline'},
+      "render_args": {'columns': [
+        ['x', 1, 2, 3],
+        ['y', 1, 2, 3],
+      ]}
+    })
     fake_generate.should.be.called()
   })
 
   it('billboard called with correct arguments', function() {
-    const fake_generate = sinon.spy()
-    const ChartComponent = ChartComponentInjector({'billboard.js': {'bb': {'generate': fake_generate}}}).default
-    const bind = ChartComponent({'type': 'spline'})
-    const d3 = require('d3')
-    const my_selection = d3.selection()
-    my_selection.append = () => ({'node': () => 'magic'})
-    const render = bind(my_selection)
-    render({'columns': [
-      ['x', 1, 2, 3],
-      ['y', 1, 2, 3],
-    ]})
+    const { fake_generate, selection } = call_render_with({
+      "component_args": {'type': 'spline'},
+      "render_args": {'columns': [
+        ['x', 1, 2, 3],
+        ['y', 1, 2, 3],
+      ]}
+    })
     fake_generate.should.be.calledWith({
-      'bindto': my_selection.append().node(),
+      'bindto': selection.append().node(),
       'data': {
         'type': 'spline',
         'columns': [
@@ -45,19 +52,15 @@ describe('ChartComponent', function() {
   })
 
   it('billboard called with correct arguments 2', function() {
-    const fake_generate = sinon.spy()
-    const ChartComponent = ChartComponentInjector({'billboard.js': {'bb': {'generate': fake_generate}}}).default
-    const bind = ChartComponent({'type': 'pie'})
-    const d3 = require('d3')
-    const my_selection = d3.selection()
-    my_selection.append = () => ({'node': () => 'magic'})
-    const render = bind(my_selection)
-    render({'columns': [
-      ['a', 1, 2, 3],
-      ['b', 1, 2, 3],
-    ]})
+    const { fake_generate, selection } = call_render_with({
+      "component_args": {'type': 'pie'},
+      "render_args": {'columns': [
+        ['a', 1, 2, 3],
+        ['b', 1, 2, 3],
+      ]}
+    })
     fake_generate.should.be.calledWith({
-      'bindto': my_selection.append().node(),
+      'bindto': selection.append().node(),
       'data': {
         'type': 'pie',
         'columns': [
--- src/components/text/Text.test.js
+++ src/components/text/Text.test.js
@@ -6,25 +6,26 @@ var jsdom = require('mocha-jsdom')
 describe('Text component', function() {
   jsdom({'useEach': true})
 
+  const get_render_function = (component_args) => {
+    const bind = TextComponent(component_args)
+    const d3 = require('d3')
+    const render = bind(d3.selection())
+    return { render, d3 }
+  }
+
   it('should throw on invalid tag name', () => {
     (() => {TextComponent({'tagName': 'foo bar'})()()})
       .should.throw('Argument \'tagName\' is invalid')
   })
 
   it('text is rendered', function() {
-    document.head.innerHTML = '<title>foobar</title>'
-    const bind = TextComponent({'tagName': 'span'})
-    const d3 = require('d3')
-    const render = bind(d3.selection())
+    const { render, d3 } = get_render_function({'tagName': 'span'})
     render('Hello World from TextComponent')
     assert.equal(d3.selection().select('span').text(), 'Hello World from TextComponent')
   })
 
   it('update function updates text', function() {
-    document.head.innerHTML = '<title>foobar</title>'
-    const bind = TextComponent({'tagName': 'span'})
-    const d3 = require('d3')
-    const render = bind(d3.selection())
+    const { render, d3 } = get_render_function({'tagName': 'span'})
     render('Hello World from TextComponent')
     render('Second version')
     assert.equal(d3.selection().select('span').text(), 'Second version')

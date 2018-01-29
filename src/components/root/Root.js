c384300f7b15479594a8e3fa8d5a97a73aad2c20
Dániel Kántor <git@daniel-kantor.com>
2018-01-29 21:18:30 +0100

    Added docs to RootComponent

--- src/components/root/Root.js
+++ src/components/root/Root.js
@@ -2,6 +2,31 @@ import Component from '../base_component.js'
 import default_parser from '../../default_parser.js'
 import { required } from '../../validators'
 
+
+/**
+ * A Component to contain the dashboard.
+ * @module components/root
+ * @param args Component arguments
+ * @param args.title Document title (`<title>`)
+ * @returns {function}
+ *
+ * @example <caption>YAML format</caption>
+ * dashboard "Hello World":
+ *   ...
+ *
+ * @example <caption>JSON format</caption>
+ * {
+ *   "component": "root",
+ *   "args": {"title": "Hello World"},
+ *   "data": [...]
+ * }
+ *
+ * @example <caption>JavaScript format</caption>
+ * import RootComponent from 'components/root'
+ *
+ * RootComponent({'title': 'Hello World'})(d3.selection())(...)
+ *
+ */
 const RootComponent = Component({
   'validators': [required('title')],
   'init': (args, selection) => selection.select('title').text(args.title),

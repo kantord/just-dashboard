dbcd959d6b3822a1703d873f8e164859880fefa5
Dániel Kántor <git@daniel-kantor.com>
2018-02-01 17:56:48 +0100

    Refactored DropdownComponent

--- src/components/dropdown/Dropdown.js
+++ src/components/dropdown/Dropdown.js
@@ -2,24 +2,34 @@ import Component from '../base_component.js'
 import { required } from '../../validators'
 import { regexp } from '../../validators'
 
+const set_values = (selection) => selection
+  .property('value', d => d.value)
+  .text(d => d.text)
+
+const enter = (selection) => selection
+  .enter()
+    .append('option')
+    .attr('class', 'ds--select-option')
+    .call(set_values)
+
+const exit = (selection) => selection
+  .exit()
+    .remove()
+
+const update = (selection) => selection
+  .call(set_values)
+
+const update_pattern = (selection) =>
+  [enter, exit, update].map(f => f(selection))
+
 const DropdownComponent = Component({
   'validators': [
     required('variable'), regexp('variable', /^[A-Za-z]([_A-Za-z0-9-]*[_A-Za-z0-9])?$/),
     required('default')],
-  'init': (args, selection) => selection.append('select').attr('class', 'ds--select'),
-  'render': (args, selection, data, item) => {
-    const join = item.selectAll('option').data(data)
-    join.enter()
-      .append('option')
-      .attr('class', 'ds--select-option')
-      .property('value', d => d.value)
-      .text(d => d.text)
-    join.exit()
-      .remove()
-    join
-      .property('value', d => d.value)
-      .text(d => d.text)
-  }
+  'init': (args, selection) => selection
+    .append('select').attr('class', 'ds--select'),
+  'render': (args, selection, data, item) => item
+    .selectAll('option').data(data).call(update_pattern)
 })
 
 export default DropdownComponent

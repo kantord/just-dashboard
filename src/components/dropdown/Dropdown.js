99576cf3f51c3bb1be053183f4f566577c85ffa7
Dániel Kántor <git@daniel-kantor.com>
2018-02-12 21:41:24 +0100

    feat: DropdownComponent reports variable values
    through args 'init_variable' and 'set_variable'

--- src/components/dropdown/Dropdown.js
+++ src/components/dropdown/Dropdown.js
@@ -1,6 +1,7 @@
 import Component from '../base_component.js'
 import { required } from '../../validators'
 import { regexp } from '../../validators'
+import * as d3 from 'd3'
 
 const set_values = (selection) => selection
   .property('value', d => d.value)
@@ -26,10 +27,20 @@ const DropdownComponent = Component({
   'validators': [
     required('variable'), regexp('variable', /^[A-Za-z]([_A-Za-z0-9-]*[_A-Za-z0-9])?$/),
     required('default')],
-  'init': (args, selection) => selection
-    .append('select').attr('class', 'ds--select'),
-  'render': (args, selection, data, item) => item
-    .selectAll('option').data(data).call(update_pattern)
+  'init': (args, selection) => {
+    args.init_variable(args.variable, args.default)
+    return selection
+      .append('select').attr('class', 'ds--select')
+  },
+  'render': (args, selection, data, item) => {
+    item
+      .selectAll('option').data(data).call(update_pattern)
+
+    item
+      .on('change', function() {
+        args.set_variable(args.variable, d3.select(this).property('value'))
+      })
+  }
 })
 
 export default DropdownComponent

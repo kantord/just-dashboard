84cfdd7414e0de0941aa74e51b85199f5d945d4d
Dániel Kántor <git@daniel-kantor.com>
2018-02-18 17:35:15 +0100

    test: Updated manual test
    Added examples that use variables in various ways

--- src/manual_test.js
+++ src/manual_test.js
@@ -3,19 +3,21 @@ import {json_parser, yaml_parser} from './index.js'
 import * as d3 from 'd3'
 
 const render_dashboard = (data) => {
-  console.log(data)
   const parserd_yaml = yaml_parser(data)
-  console.log(parserd_yaml)
   json_parser(parserd_yaml)(d3.selection())
 }
 
 render_dashboard(`
 dashboard "Cereals":
   - h1 text: "Cereals"
-  - dropdown foo=bar:
-    - {"value": "bar", "text": "bar"}
-    - {"value": "baz", "text": "baz"}
-  - h2 text: "By calories"
+  - dropdown foo=h2:
+    - {"value": "h2", "text": "h2"}
+    - {"value": "p", "text": "p"}
+  - \${foo} text: "By calories"
+  - h3 text: "Cereals \${foo}"
+  - columns:
+    - h3 text: "Cereals \${foo}"
+    - h3 text: "Cereals \${foo}"
 `)
 
 //render_dashboard(

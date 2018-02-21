9d35ad81a532224d5f4a11f0273180382d84d208
Dániel Kántor <git@daniel-kantor.com>
2018-02-21 17:40:52 +0100

    test: Added end to end test
    .
    
    test: Added end to end test
    
    test: updated manual test
    
    test: Added integration.test.js
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    chore: removed some unused packages
    
    tests: Removed unused integration.test.js
    
    Fixed manual test
    
    test: fixed manual_test.js

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -3,7 +3,7 @@ const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   entry: './src/index.js',
-  target: 'web',
+  target: 'node',
   externals: [nodeExternals()],
   output: {
     filename: 'bundle.js',

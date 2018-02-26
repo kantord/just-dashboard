7f9418c50821b0b61e12345c4a0fced1c72c9c9e
Dániel Kántor <git@daniel-kantor.com>
2018-02-26 19:03:14 +0100

    test: fixed manual_test.js

--- src/manual_test.js
+++ src/manual_test.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-alert, no-console */
+/* eslint-disable */
 import {json_parser, yaml_parser} from './index.js'
 import * as d3 from 'd3'
 
@@ -10,12 +10,12 @@ const render_dashboard = (data) => {
 render_dashboard(`
 dashboard "Cereals":
   - h1 text: "Cereals"
-  - dropdown foo=h2:
-    - {"value": "h2", "text": "h2"}
-    - {"value": "p", "text": "p"}
-  - \${foo} text: "By calories"
-  - h3 text: "Cereals \${foo}"
-  - columns:
-    - h3 text: "Cereals \${foo}"
-    - h3 text: "Cereals \${foo}"
+  - h2 text: "By calories"
+  - bar chart:
+    - attr:query: '{"columns": [(sort_by(-(.calories | tonumber)) | .[] | [.name, .calories])]}'
+    - data: https://gist.githubusercontent.com/ZeningQu/6184eaf8faa533e320abc938c4738c3e/raw/40f237de825061faa8721c2293b79c46979780b4/cereals.csv
+  - h2 text: "By nutritional profile"
+  - 4 columns:
+    - attr:query: '.[] | {"component": "rows", "data": [{"component": "text", "args": {"tagName": "h3"}, "data": .name}, {"component": "chart", "args": {"type": "pie"}, "data": {"columns": [["protein", .protein], ["carbo", .carbo], ["sugars", .sugars], ["fat", .fat]]}}]}'
+    - data: https://gist.githubusercontent.com/ZeningQu/6184eaf8faa533e320abc938c4738c3e/raw/40f237de825061faa8721c2293b79c46979780b4/cereals.csv
 `)

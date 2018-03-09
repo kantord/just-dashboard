a3c2c59dc0c341221a98aa331810d3eb8d5fa027
Dániel Kántor <git@daniel-kantor.com>
2018-03-09 16:43:53 +0100

    docs(components/Dropdown): Added documentation for DropdownComponent
    fixes #41

--- src/components/dropdown/Dropdown.js
+++ src/components/dropdown/Dropdown.js
@@ -23,6 +23,40 @@ const update = (selection) => selection
 const update_pattern = (selection) =>
   [enter, exit, update].map(f => f(selection))
 
+/**
+ * Creates a dropdown
+ * @module components/dropdown
+ * @param args Component arguments
+ * @param args.variable Variable name
+ * @param args.default Default value
+ * @returns {function}
+ *
+ * @example <caption>YAML format</caption>
+ * dropdown name=JohnDoe:
+ *   - {"value": "JohnDoe", "text": "John Doe"}
+ *   - {"value": "JoeSmith", "text": "Joe Smith"}
+ *
+ * @example
+ * bar dropdown:
+ *   ...
+ *
+ * @example <caption>JSON format</caption>
+ * {
+ *   "component": "dropdown",
+ *   "args": {"variable": "name", "default": "JohnDoe"},
+ *   "data": [
+ *     {"value": "JohnDoe", "text": "John Doe"},
+ *     {"value": "JoeSmith", "text": "Joe Smith"}
+ *   ]
+ * }
+ *
+ * @example <caption>JavaScript format</caption>
+ * import DropdownComponent from 'components/dropdown'
+ *
+ * DropdownComponent({"variable": "name", "default": "JohnDoe"})(d3.selection())(...)
+ *
+ *
+ */
 const DropdownComponent = Component({
   'validators': [
     required('variable'),

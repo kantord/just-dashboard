4cc2d8448c31d9d31155e45ad114c3f169e02276
Dániel Kántor <git@daniel-kantor.com>
2018-03-12 17:06:13 +0100

    feat: export d3 in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.scss'
 import json_parser from './default_parser'
 import yaml_parser from './yaml-format/parser'
+import * as d3 from 'd3'
 
-export { json_parser, yaml_parser }
+export { json_parser, yaml_parser, d3 }

c51c5a909e2f55b8cdb43a138f547d42860e1bd1
Dániel Kántor <git@daniel-kantor.com>
2018-03-21 22:07:34 +0100

    feat: Add 'file_loader' option to json parser

--- src/parser/parser.js
+++ src/parser/parser.js
@@ -4,15 +4,17 @@ import create_state_handler from '../state_handler.js'
 /** Creates a function that parses a JSON component and compiles it into a
  * Javascript component
   * @param {Function} component_loader - A function that can load components*/
-const parse = (component_loader) => (input) => {
+const parse = (component_loader) => (input, file_loader) => {
   if (!(typeof input === 'object')) throw new Error('An object is required')
   validators.required('component')(input)
   validators.regexp('component', /^[A-z]\w*$/)(input)
 
   const component = component_loader(input.component)
   const args = Object.assign({}, input.args)
-  if (input.component === 'root')
+  if (input.component === 'root') {
     args.state_handler = create_state_handler()
+    args.file_loader = file_loader
+  }
   const bind = component(args)
 
   return (selection) => {

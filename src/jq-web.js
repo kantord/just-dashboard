33e0871558125762f85c84db77ca8f5ed3242a2d
Daniel Kantor <git@daniel-kantor.com>
2026-02-20 19:21:23 +0100

    build: replace webpack with vite

--- src/jq-web.js
+++ src/jq-web.js
@@ -1,6 +1,6 @@
 
 const emuto = async (data, query) => {
-  const emuto_ = await import(/* webpackChunkName: "emuto" */ 'emuto/lib/interpreter')
+  const emuto_ = await import('emuto/lib/interpreter')
 	console.log(emuto_) // eslint-disable-line
   return emuto_.default(query)(data)
 }
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/index.js',
+      name: 'dashboard',
+      formats: ['umd', 'es'],
+      fileName: (format) => format === 'umd' ? 'dashboard.js' : 'dashboard.es.js',
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',

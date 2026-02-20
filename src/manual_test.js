8d58ab53f99b2a33cff09dabd49f1e27485f82e2
Daniel Kantor <git@daniel-kantor.com>
2026-02-20 19:05:08 +0100

    chore: update some dependencies

--- src/manual_test.js
+++ src/manual_test.js
@@ -2,19 +2,29 @@
 import {json_parser, yaml_parser} from './index.js'
 import * as d3 from 'd3'
 
+const cereals_csv = `name,calories,protein,fat,carbo
+Corn Flakes,100,2,0,21
+Frosted Flakes,110,1,0,26
+Raisin Bran,120,3,1,14
+Cheerios,110,6,2,17
+Special K,110,6,0,16
+Rice Krispies,130,2,0,22
+Grape Nuts,110,3,0,17
+Froot Loops,110,2,1,25`
+
 const render_dashboard = (data) => {
   const parserd_yaml = yaml_parser(data)
   const file_loader = (path, callback) => {
-    callback(undefined, 'a,b\nCereals,0')
+    callback(undefined, cereals_csv)
   }
   json_parser(parserd_yaml, file_loader)(d3.selection())
 }
 
 render_dashboard(`
 dashboard "Cereals":
-  - h1 text: 
-    - attr:query: '$[0].a'
-    - data: file:///foo/bar.csv
+  - h1 text:
+    - attr:query: '$[0].name'
+    - data: file:///cereals.csv
   - h2 text: "By calories"
   - dropdown my_var=~last:
     - {"value": "foo", "text": "Foo"}
@@ -24,9 +34,9 @@ dashboard "Cereals":
     - p text: "foo \${my_var} bar"
   - bar chart:
     - attr:query: 'map ($ => [$.name, $.calories * 1]) | sortBy ($ => $[1] * -1) | { "columns": $ }'
-    - data: https://gist.githubusercontent.com/ZeningQu/6184eaf8faa533e320abc938c4738c3e/raw/40f237de825061faa8721c2293b79c46979780b4/cereals.csv
+    - data: file:///cereals.csv
   - h2 text: "By nutritional profile"
   - 4 columns:
     - attr:query: 'map ($ => {"component": "rows", "data": [{"component": "text", "args": {"tagName": "h3"}, "data": .name}, {"component": "chart", "args": {"type": "pie"}, "data": {"columns": ["protein": .protein, "fat": .fat, "carbo": .carbo]}}]})'
-    - data: https://gist.githubusercontent.com/ZeningQu/6184eaf8faa533e320abc938c4738c3e/raw/40f237de825061faa8721c2293b79c46979780b4/cereals.csv
+    - data: file:///cereals.csv
 `)
